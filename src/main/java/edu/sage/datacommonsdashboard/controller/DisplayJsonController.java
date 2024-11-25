9047eea48f9df0a3b10f949028c78ded0bd535b5
Christy Grant <cgrant@ucar.edu>
2024-11-25 09:30:35 -0700

    Fix copy/paste error to point to derecho data.

--- src/main/java/edu/sage/datacommonsdashboard/controller/DisplayJsonController.java
+++ src/main/java/edu/sage/datacommonsdashboard/controller/DisplayJsonController.java
@@ -92,7 +92,7 @@ public class DisplayJsonController {
         headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
 
         try {
-            String jsonData = fileRepository.getCasperQstatJobsJson();
+            String jsonData = fileRepository.getDerechoQstatJobsJson();
             return ResponseEntity.ok().headers(headers).body(jsonData);
 
         } catch (IOException e) {
