5c2a73e5979ea3b2371b597ea795e2d1c175026f
Christy Grant <cgrant@ucar.edu>
2024-12-02 11:03:04 -0700

    Minimal test for text controller method to get casper jobs text page.

---
+++ src/test/java/edu/sage/datacommonsdashboard/controller/DisplayTextControllerTest.java
@@ -0,0 +1,32 @@
+package edu.sage.datacommonsdashboard.controller;
+
+import edu.sage.datacommonsdashboard.repository.FileRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class DisplayTextControllerTest {
+
+    @MockBean
+    private FileRepository mockFileRepository;
+
+    @MockBean
+    private Model mockModel;
+
+    @Test
+    public void when_request_casper_jobs_text__then_get_view_name() throws Exception {
+
+        when(mockFileRepository.getCasperQstatJobsText()).thenReturn("mockTextOutput");
+
+        DisplayTextController controller = new DisplayTextController(mockFileRepository);
+
+        String viewName = controller.showCasperPage(mockModel);
+        assertEquals("display-queue-data", viewName);
+    }
+
+}
