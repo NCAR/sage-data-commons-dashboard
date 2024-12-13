05a4310c82e03786094d72f260ed78bde9d445f4
Christy Grant <cgrant@ucar.edu>
2024-12-12 18:09:42 -0700

    DCOMM-67 Create a Dashboard Contents Page with Links to Output Pages

---
+++ src/main/java/edu/sage/datacommonsdashboard/controller/HomeController.java
@@ -0,0 +1,15 @@
+package edu.sage.datacommonsdashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        model.addAttribute("pageTitle", "Home Page");
+        return "index"; // Corresponds to src/main/resources/templates/index.html
+    }
+}
