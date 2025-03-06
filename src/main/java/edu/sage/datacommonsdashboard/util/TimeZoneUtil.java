6dd03713c544d715a2a869c6dc8b404959328c9b
Christy Grant <cgrant@ucar.edu>
2025-03-06 16:56:02 -0700

    DCOMM-122 Removed unused methods and tests.

--- src/main/java/edu/sage/datacommonsdashboard/util/TimeZoneUtil.java
+++ src/main/java/edu/sage/datacommonsdashboard/util/TimeZoneUtil.java
@@ -10,47 +10,9 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class TimeZoneUtil {
 
-    public ZoneId getCurrentServerZoneId() {
-        // Get default time zone of the server (e.g. "America/Denver")
-        return ZoneId.systemDefault();
-    }
-
-    public String getAbbreviationNowInDenverTimeZone() {
-
-        // Get the current date and time in Denver's time zone
-        ZonedDateTime nowInDenver = ZonedDateTime.now(ZoneId.of("America/Denver"));
-
-        /// Use the DateTimeFormatter with the z pattern to get the abbreviation
-        // String timeZoneName = nowInDenver.getZone().getId();  // Always "America/Denver"
-        String abbreviation = nowInDenver.format(java.time.format.DateTimeFormatter.ofPattern("z"));
-
-        return abbreviation;
-    }
-
-    // E.g. MST vs MDT current system
-    public String getAbbreviationInTimeZone(ZoneId zoneId) {
-
-        // Get the current date and time in server's time zone
-        ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
-
-        /// Use the DateTimeFormatter with the z pattern to get the abbreviation
-        String abbreviation = zonedDateTime.format(java.time.format.DateTimeFormatter.ofPattern("z"));
-
-        return abbreviation;
-    }
-
-    public String getAbbreviationForZonedDateTime(ZonedDateTime zonedDateTime) {
-
-        // Use the DateTimeFormatter to get the abbreviation
-        String abbreviation = zonedDateTime.format(java.time.format.DateTimeFormatter.ofPattern("z"));
-
-        return abbreviation;
-    }
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss z";
 
     public String convertTimestampToDateString(Integer timestamp, ZoneId zoneId) {
-
-        //ZoneId denverZone = ZoneId.of("America/Denver");
-       // ZoneId currentZone = getCurrentServerZoneId();
 
         // Timestamp is in seconds, needs converting to millis
         Instant instant = Instant.ofEpochMilli(timestamp * 1000L);
@@ -58,8 +20,7 @@ public class TimeZoneUtil {
         // Convert the Instant to a ZonedDateTime in the Time Zone
         ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, zoneId);
 
-        // Format the ZonedDateTime
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 
         return formatter.format(zonedDateTime);
     }
